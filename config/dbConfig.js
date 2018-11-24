520b0ee29cbb03a11e6e5a1c5566e2f412f5efd0
Sarah Phillips <snphillips@gmail.com>
2018-11-24 12:30:53 -0500

    heroku

---
+++ config/dbConfig.js
@@ -0,0 +1,34 @@
+
+
+// This file contains all the connection strings to connect
+// to the database server
+
+
+
+// const Pool = require('pg').Pool
+
+// const pool = process.env.DATABASE_URL || new Pool({
+//   user: process.env.USER,
+//   host: process.env.DB_HOST || 'localhost',
+//   database: process.env.DATABASE,
+//   password: process.env.PW,
+//   port: process.env.DB_PORT || 5432,
+// })
+
+// module.exports = pool;
+
+
+
+const Pool = require('pg').Pool
+
+module.exports = process.env.DATABASE_URL || new Pool ({
+  user: process.env.USER,
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DATABASE,
+  password: process.env.PW,
+  port: process.env.DB_PORT || 5432,
+})
+
+
+
+
