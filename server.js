a9e2ccf88b6f0c6cbd1a92c8f7f5c9fd7fe39e24
Sarah Phillips <snphillips@gmail.com>
2018-11-25 14:34:13 -0500

    heroku master

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ require('dotenv').config()
 //require the just installed express app
 const express = require('express');
 
+const cors = require('cors')
+
 //invoke express. Henceforth, app = express
 const app = express();
-
-
-const db = require('./queries')
 
 const { DATABASE_URL } = process.env;
 
 const bodyParser = require('body-parser');
-const cors = require('cors')
+
+const db = require('./queries')
 
 
 // ==================================
@@ -28,6 +28,7 @@ const axios = require('axios');
         const { Client } = require('pg');
 
         const client = new Client({
+
           connectionString: process.env.DATABASE_URL,
           ssl: true
         });
