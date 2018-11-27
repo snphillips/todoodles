6d37a367602ea9c7884ed48583f5d589a88aed99
Sarah Phillips <snphillips@gmail.com>
2018-11-27 08:57:32 -0500

    style footer & media query

--- client/src/Header.js
+++ client/src/Header.js
@@ -4,9 +4,9 @@ export default class Header extends Component {
   render() {
     return (
 
-    <div>
+    <div className="header">
 
-      <h1 className="header">todoodles</h1>
+      <h1>todoodles</h1>
 
       <img className="scribble"
            src="https://i.imgur.com/EGh0f7V.png"
