d3c6ea1898d3d46f67e0351195b0722ba8ddecbc
christinavoudouris <christina.voudouris@gmail.com>
2020-05-11 14:05:12 -0400

    bumped packages, installed react-moment

--- client/src/DeleteButton.js
+++ client/src/DeleteButton.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 export default function DeleteButton(props) {
   return (
-    <>
-      <input
-        className="button"
-        type="submit"
-        value="remove from list"
-        onClick={props.handleSubmitDelete}
-      />
-    </>
+    <input
+      className="button"
+      type="submit"
+      value="remove from list"
+      onClick={props.handleSubmitDelete}
+    />
   );
 }
